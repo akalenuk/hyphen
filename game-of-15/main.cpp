9f5aacc1b89797ac17572406092e15246813b555
Alexandr Kalenuk <akalenuk@gmail.com>
2014-11-11 21:47:43 +0200

    Little cleanup

--- game-of-15/main.cpp
+++ game-of-15/main.cpp
@@ -12,7 +12,7 @@ using namespace hyphen;
 
 HGE*        hge = 0;
 hgeQuad     bg;
-HTEXTURE	cursor_texture;
+HTEXTURE    cursor_texture;
 hgeSprite*  cursor_sprite;
 hgeGUI*     gui;
 
@@ -20,19 +20,23 @@ bool FrameFunc()
 {
     float dt=hge->Timer_GetDelta();
 
-	if (hge->Input_GetKeyState(HGEK_ESCAPE)) return true;
+    if (hge->Input_GetKeyState(HGEK_ESCAPE)) return true;
 
-	bg.v[0].x=0;    bg.v[0].y=0;
-	bg.v[1].x=800;  bg.v[1].y=0;
-	bg.v[2].x=800;  bg.v[2].y=600;
-	bg.v[3].x=0;    bg.v[3].y=600;
+    bg.v[0].x=0;    bg.v[0].y=0;
+    bg.v[1].x=800;  bg.v[1].y=0;
+    bg.v[2].x=800;  bg.v[2].y=600;
+    bg.v[3].x=0;    bg.v[3].y=600;
 
     vector<Thing>::iterator iter = context::get_things().begin();
-	vector<Thing>::iterator eit = context::get_things().end();
-	for(; iter != eit; ++iter){
+    vector<Thing>::iterator eit = context::get_things().end();
+    for(; iter != eit; ++iter){
         (*iter).Update(dt);
-	}
+    }
 
+
+    //
+    // here's the whole code for the game logic
+    //
     for(the I : from-1-to-15){
         the Tile = "tile_" & I;
         the EmptyTileX = "tile_16"-x;
@@ -46,50 +50,56 @@ bool FrameFunc()
     }
     teleport-"cursor"-to-(mouse-x)-(mouse-y);
 
-	return false;
+
+
+    return false;
 }
 
 bool RenderFunc()
 {
-	hge->Gfx_BeginScene();
-	hge->Gfx_Clear(0);
+    hge->Gfx_BeginScene();
+    hge->Gfx_Clear(0);
 
-	hge->Gfx_RenderQuad(&bg);
-	vector<Thing>::iterator it = context::get_things().begin();
-	vector<Thing>::iterator eit = context::get_things().end();
-	for(; it != eit; ++it){
+    hge->Gfx_RenderQuad(&bg);
+    vector<Thing>::iterator it = context::get_things().begin();
+    vector<Thing>::iterator eit = context::get_things().end();
+    for(; it != eit; ++it){
         (*it).Render();
-	}
+    }
 
-	hge->Gfx_EndScene();
-	return false;
+    hge->Gfx_EndScene();
+    return false;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	hge = hgeCreate(HGE_VERSION);
-	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
-	hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
+    hge = hgeCreate(HGE_VERSION);
+    hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
+    hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
 
-	hge->System_SetState(HGE_WINDOWED, true);
-	hge->System_SetState(HGE_SCREENWIDTH, 800);
-	hge->System_SetState(HGE_SCREENHEIGHT, 600);
-	hge->System_SetState(HGE_SCREENBPP, 32);
+    hge->System_SetState(HGE_WINDOWED, true);
+    hge->System_SetState(HGE_SCREENWIDTH, 800);
+    hge->System_SetState(HGE_SCREENHEIGHT, 600);
+    hge->System_SetState(HGE_SCREENBPP, 32);
 
-	if(hge->System_Initiate()){
+    if(hge->System_Initiate()){
         context::set_hge(hge);
 
         bg.tex = hge->Texture_Load("stuff/bg.png");
         bg.blend = BLEND_ALPHAADD | BLEND_COLORMUL | BLEND_ZWRITE;
-		for(int i=0;i<4;i++){
-			bg.v[i].z=0.5f;
-			bg.v[i].col=0xFFFFA000;
-		}
-		bg.v[0].tx=0.0f; bg.v[0].ty=0.0f;
-		bg.v[1].tx=1.0f; bg.v[1].ty=0.0f;
-		bg.v[2].tx=1.0f; bg.v[2].ty=1.0f;
-		bg.v[3].tx=0.0f; bg.v[3].ty=1.0f;
+        for(int i=0;i<4;i++){
+            bg.v[i].z=0.5f;
+            bg.v[i].col=0xFFFFA000;
+        }
+        bg.v[0].tx=0.0f; bg.v[0].ty=0.0f;
+        bg.v[1].tx=1.0f; bg.v[1].ty=0.0f;
+        bg.v[2].tx=1.0f; bg.v[2].ty=1.0f;
+        bg.v[3].tx=0.0f; bg.v[3].ty=1.0f;
 
+
+        //
+        // here's the whole code for the game initializing
+        //
         for(the I : from-1-to-4){
             for(the J : from-1-to-4){
                 the K = I * 4 + J - 4;
@@ -106,21 +116,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         }
         load-"stuff/cursor.png"-as-"cursor";
 
-		hge->System_Start();
 
-		hge->Texture_Free(bg.tex);
-		vector<Thing>::iterator it = context::get_things().begin();
+
+        hge->System_Start();
+
+        hge->Texture_Free(bg.tex);
+        vector<Thing>::iterator it = context::get_things().begin();
         vector<Thing>::iterator eit = context::get_things().end();
         for(; it != eit; ++it){
             if(it->visible){
                 context::get_hge()->Texture_Free( (*it).picture.tex );
             }
         }
-	}else{
-		MessageBox(NULL, hge->System_GetErrorMessage(), "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
-	}
-	hge->System_Shutdown();
-	hge->Release();
-	return 0;
+    }else{
+        MessageBox(NULL, hge->System_GetErrorMessage(), "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
+    }
+    hge->System_Shutdown();
+    hge->Release();
+    return 0;
 }
 
